274ec9c410bcd1df7286a38fe1f1909c133de874
Masseix Kévin <masseix.kevin@gmail.com>
2017-12-05 21:39:18 +0100

    Fix space after token YIELD_FROM introduced by php 7.1

--- fix-fmt.php
+++ fix-fmt.php
@@ -6,13 +6,31 @@ $tmpDir = __DIR__;
 
 (new Phar($pharPath))->extractTo($tmpDir);
 
+$lines = explode("\n", file_get_contents('fmt.stub.php'));
+
+// FIX PHP7.1 yield from : https://github.com/nanch/phpfmt_stable/issues/37
+$toAdd = [
+    "",
+    "\t\t\tcase T_YIELD_FROM:",
+    "\t\t\t\t\$this->appendCode(\$text . \$this->getSpace(\$this->rightTokenIs(T_STRING)));",
+    "\t\t\t\tbreak;",
+];
+
+if (array_search($toAdd[1], $lines) === false) {
+    foreach ($toAdd as $key => $value) {
+        array_splice($lines, 5227 + $key, 0, $value);
+    }
+}
+
+// FIX PHP7.1 nullable types : https://github.com/nanch/phpfmt_stable/issues/36
+$nullableLine = array_search("\t\t\t\t\t\$this->appendCode(' ' . \$text . \$this->getSpace(!\$this->rightTokenIs(ST_COLON)));", $lines);
+if ($nullableLine !== false) {
+    $lines[$nullableLine] = "\t\t\t\t\t\$this->appendCode(' ' . \$text . \$this->getSpace(!\$this->rightTokenIs(ST_COLON) && (\$id !== ST_QUESTION || !\$this->rightTokenIs(T_STRING))));";
+}
+
 file_put_contents(
     'fmt.stub.php',
-    str_replace(
-        "\$this->appendCode(' ' . \$text . \$this->getSpace(!\$this->rightTokenIs(ST_COLON)));",
-        "\$this->appendCode(' ' . \$text . \$this->getSpace(!\$this->rightTokenIs(ST_COLON) && (\$id !== ST_QUESTION || !\$this->rightTokenIs(T_STRING))));",
-        file_get_contents('fmt.stub.php')
-    )
+    implode("\n", $lines)
 );
 
 $phar = new Phar($pharPath);
