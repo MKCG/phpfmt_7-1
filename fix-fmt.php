b1ef4b135f2b637afa9fe1abe2d1552b43acadd0
Masseix Kévin <masseix.kevin@gmail.com>
2017-12-06 17:04:58 +0100

    Fix modifier visibility and keywords lowercased when used as method name

--- fix-fmt.php
+++ fix-fmt.php
@@ -26,19 +26,50 @@ $patchs = [
     // FIX PHP7.1 nullable types : https://github.com/nanch/phpfmt_stable/issues/36
     [
         "\t\t\t\t\t\$this->appendCode(' ' . \$text . \$this->getSpace(!\$this->rightTokenIs(ST_COLON)));",
-        "\t\t\t\t\t\$this->appendCode(' ' . \$text . \$this->getSpace(!\$this->rightTokenIs(ST_COLON) && (\$id !== ST_QUESTION || !\$this->rightTokenIs(T_STRING))));",
+        "\t\t\t\t\t\$this->appendCode(\$this->getSpace(\$id !== ST_QUESTION || !\$this->leftMemoUsefulTokenIs(ST_PARENTHESES_OPEN)) . \$text . \$this->getSpace(!\$this->rightTokenIs(ST_COLON) && (\$id !== ST_QUESTION || !\$this->rightTokenIs(T_STRING))));",
     ],
     // FIX pass ClassToStatic : https://github.com/nanch/phpfmt_stable/issues/31
     [
         "\t\t\t\t\$this->tkns[\$i] = [T_STRING, self::PLACEHOLDER];",
         "\t\t\t\t\$this->tkns[\$i] = [T_STRING, static::PLACEHOLDER];",
     ],
+    // Fix : PHP keyword used as function names must not be lowercased since PHP7
+    [
+        "\t\t\t\tT_CONSTANT_ENCAPSED_STRING == \$id",
+        "\t\t\t\tT_CONSTANT_ENCAPSED_STRING == \$id || T_STRING == \$id"
+    ]
 ];
 
 foreach ($patchs as list($search, $replacement)) {
     $lineNumber = array_search($search, $lines);
     if ($lineNumber) {
         $lines[$lineNumber] = $replacement;
+    }
+}
+
+$pathTokenParse = [
+    // Fix : PHP keyword used as function names must not be lowercased since PHP7
+    [
+        "final class PSR2KeywordsLowerCase extends FormatterPass {",
+        "\tpublic function format(\$source) {",
+        "\t\t\$this->tkns = token_get_all(\$source, TOKEN_PARSE);"
+    ],
+    // Fix : visibility modifier is preserved when method names is a PHP keyword : https://github.com/nanch/phpfmt_stable/issues/19
+    [
+        "final class PSR2ModifierVisibilityStaticOrder extends FormatterPass {",
+        "\tpublic function format(\$source) {",
+        "\t\t\$this->tkns = token_get_all(\$source, TOKEN_PARSE);"
+    ],
+];
+
+foreach ($pathTokenParse as list($search, $methodStart, $replacement)) {
+    $lineNumber = array_search($search, $lines);
+
+    for ($i = $lineNumber; isset($lines[$i]); $i++) {
+        if ($lines[$i] === $methodStart) {
+            $lines[$i+1] = $replacement;
+            break;
+        }
     }
 }
 
